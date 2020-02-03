4744c0edc4502d98d7e5c0c00451749c09a2a23f
Reginald Richardson <youngreg21@gmail.com>
2020-02-03 08:47:03 -0500

    First commit of Task List CapStone

---
+++ Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Capstone_TaskList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Continue = true;
+            int switchCase;
+                
+            List<string> errorMessages = new List<string>
+            {
+                new string("Value is not a number or at least I can't convert it!! Returning to main menu"),
+                new string("Invalid input of "),
+                new string("Invalid input. Please enter y to continue or n to exit: ")
+            };
+            string userOptionToTest = " ";
+            int userOption;
+            List<TaskList> tasks = new List<TaskList>
+            {
+                new TaskList("Reginald Richardson", "Create the Print Menu for Tasks Capstone", DateTime.Parse("01/31/2020"), true, 1),
+                new TaskList("Reginald Richardson", "Create the Task class", DateTime.Parse("01/31/2020"), true, 2),
+                new TaskList("Reginald Richardson", "Create the default Task list in my program", DateTime.Parse("01/31/2020"), true, 3),
+                new TaskList("Reginald Richardson", "Create the validation methods for user input", DateTime.Parse("01/31/2020"), true, 4),
+                new TaskList("Reginald Richardson", "Create the add task method", DateTime.Parse("01/31/2020"), true, 5)
+            };
+
+            while (Continue)
+            {
+                PrintMenu();
+                userOptionToTest = SelectTaskOption("ENTER YOUR OPTION: ");
+                userOption = TaskOptSelectValidation(errorMessages, userOptionToTest, 1, 5);
+                
+                switch(userOption)
+                {
+                    case 1:
+                        Console.Clear();
+                        TaskList.ListTasks(tasks);
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.Write("\n");
+                        Continue = ContinueProgram(errorMessages[2], SelectTaskOption("Would you like to continue the program?: "));
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    case 2:
+                        Console.Clear();
+                        TaskList.AddTask(tasks);
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"New task added successfully:\n{tasks[tasks.Count - 1].Name}\n" +
+                            $"{tasks[tasks.Count - 1].Description}\n{tasks[tasks.Count - 1].DueDate}\n{tasks[tasks.Count - 1].Completed}\n");
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.Write("\n");
+                        Continue = ContinueProgram(errorMessages[2], SelectTaskOption("Would you like to continue the program?: "));
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+
+                    case 3:
+                        int i = 1;
+                        int originalCount = tasks.Count;
+                        int taskToDelete;
+                        foreach(TaskList task in tasks)
+                        {
+                            Console.WriteLine($"{i}. {task.Description}");
+                            i++;
+                        }
+                        taskToDelete = TaskOptSelectValidation(errorMessages, SelectTaskOption("Enter in the corresponding number for the task you would like to delete: "), 1, tasks.Count);
+                        TaskList.DeleteTask(errorMessages[2],taskToDelete -1, tasks);
+                        if (originalCount > tasks.Count)
+                        {
+                            Console.WriteLine($"Task {taskToDelete} deleted successfully");
+                        }
+                        break;
+                    case 5:
+                        Continue = false;
+                        break;
+                }
+            }
+        }
+
+        public static void PrintMenu()
+        {
+            Console.WriteLine("TASK MANAGER MENU\n===========================\n1. List Tasks\n2. Add Task\n3. Delete Task\n4. Mark Task Complete\n5. Quit\n");
+        }
+
+        public static string SelectTaskOption(string message)
+        {
+            string userInput;
+            Console.Write(message);
+            userInput = Console.ReadLine();
+            return userInput;
+        }
+
+        public static int TaskOptSelectValidation(List<string> errorMessages, string userInput, int option1, int optionMax)
+        {
+            int userOption;
+            try
+            {
+                userOption = int.Parse(userInput);
+            }
+
+            catch(FormatException)
+            {
+                string userOptionError;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Clear();
+                Console.WriteLine(errorMessages[0]);
+                Thread.Sleep(3500);
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.White;
+                PrintMenu();
+                userOptionError = SelectTaskOption("ENTER YOUR OPTION: ");
+                return TaskOptSelectValidation(errorMessages, userOptionError, 1, 5);
+            }
+
+            if(userOption >= option1 && userOption <= optionMax)
+            {
+                return userOption;
+            }
+            else
+            {
+                string userOptionError;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Clear();
+                Console.WriteLine($"{errorMessages[1]}{userOption}.");
+                Thread.Sleep(3500);
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.White;
+                PrintMenu();
+                userOptionError = SelectTaskOption("ENTER YOUR OPTION: ");
+                return TaskOptSelectValidation(errorMessages, userOptionError, 1, 5);
+            }
+            return userOption;
+        }
+
+        public static bool ContinueProgram(string errorMessage, string userInput)
+        {
+            if(userInput == "y")
+            {
+                Console.Clear();
+                return true;
+            }
+
+            else if(userInput == "n")
+            {
+                return false;
+            }
+
+            else
+            {
+                return ContinueProgram(errorMessage, SelectTaskOption(errorMessage));
+            }
+        }
+    }
+    
+}
---
+++ TaskList.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Capstone_TaskList
+{
+    /*Team member’s name
+2. Brief description
+3. Due date
+4. Whether it’s been completed or not*/
+    class TaskList
+    {
+        private int taskNum;
+        private string name;
+        private string description;
+        private DateTime dueDate;
+        private bool completed;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public DateTime DueDate
+        {
+            get { return dueDate; }
+            set { dueDate = value; }
+        }
+        public bool Completed
+        {
+            get { return completed; }
+            set { completed = value; }
+        }
+
+        public int TaskNum
+        {
+            get { return taskNum; }
+            set { taskNum = value; }
+        }
+
+        public TaskList()
+        {
+
+        }
+
+        public TaskList(string name, string description, int taskNum)
+        {
+            this.name = name;
+            this.description = description;
+            this.taskNum = taskNum;
+        }
+
+        public TaskList(string name, string description, DateTime dueDate, bool completed, int taskNum)
+        {
+            this.name = name;
+            this.description = description;
+            this.dueDate = dueDate;
+            this.completed = completed;
+            this.taskNum = taskNum;
+        }
+
+        public static void ListTasks(List<TaskList> listOfTask)
+        {
+            foreach(TaskList items in listOfTask)
+            {
+                Console.WriteLine($"Task{items.taskNum}:\n     Assigned to: {items.name}\n     Description: {items.description}\n     Due Date: {items.dueDate}\n     Completed: {items.completed}");
+            }
+        }
+
+        public static List<TaskList> AddTask(List<TaskList> listOfTask)
+        {
+            string name;
+            string description;
+            DateTime dueDate;
+            bool isCompleted = false;
+            int taskNum = listOfTask.Count + 1;
+
+            name = CheckUserNameFormat(newTaskData("Who will this task be assigned to (enter in first and last name.  i.e Jon Doe): "));
+            description = newTaskData("Please enter in a description for this task: ");
+            dueDate = Date_D(newTaskData("What is the due date for this task (format should match xx/xx/xxxx): "));
+
+
+            TaskList newItem = new TaskList(name, description, dueDate, isCompleted, taskNum);
+            listOfTask.Add(newItem);
+
+            return listOfTask;
+        }
+
+        public static List<TaskList> DeleteTask(string errorMessage, int itemNum, List<TaskList> listOfTasks)
+        {
+            string userInput;
+            userInput = newTaskData($"Are you sure you want to delete this task:\n{listOfTasks[itemNum].Name}\n{listOfTasks[itemNum].Description}\nDue on: {listOfTasks[itemNum].Description}?");
+
+
+            if (userInput == "y")
+            {
+                listOfTasks.RemoveAt(itemNum);
+                return listOfTasks;
+            }
+
+            else
+            {
+                Console.WriteLine("Cancelling the delete operation");
+                return listOfTasks;
+            }
+        }
+
+        private static string CheckUserNameFormat(string name)
+        {
+            if (!Regex.IsMatch(name, @"([A-Z])([a-z]+) ([A-Z])([a-z]){1,30}"))
+            {
+                return CheckUserNameFormat(newTaskData("Invalid format for name entry.  (ex.  Reginald Richardson).\nPlease type in the name again with the specified format"));
+            }
+
+            else
+            {
+                return name;
+            }
+        }
+
+        public static bool ConfirmDelete(string errorMessage, string userInput)
+        {
+            if (userInput == "y")
+            {
+                Console.Clear();
+                return true;
+            }
+
+            else if (userInput == "n")
+            {
+                return false;
+            }
+
+            else
+            {
+                return ConfirmDelete(errorMessage, newTaskData(errorMessage));
+            }
+        }
+
+            public static DateTime Date_D(string date)
+        {
+            int count = 0;
+            string userInput = "";
+            DateTime newDate;
+            //check if date is properly formatted (i.e. xx/xx/xxxx)
+            if (!Regex.IsMatch(date, @"([0-9]){2}[/]([0-9]{1,2})([/])([0-9]){4}") && count < 3)
+            {
+                Console.Write("Invalid format.  Please enter in a date ex.(09/21/2022):  ");
+                userInput = Console.ReadLine();
+                count++;
+                return Date_D(userInput);
+            }
+
+            //returns the input if correctly formatted
+            else
+            {
+                newDate = DateTime.Parse(date);
+                return newDate;
+            }
+        }
+
+        private static string newTaskData(string message)
+        {
+            string userInput;
+            Console.Write(message);
+            userInput = Console.ReadLine();
+            return userInput;
+        }
+
+    }
+}
